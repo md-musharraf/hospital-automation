687fc5947b7fbba2d66d8c5e439a2ec47ad05b4f
Md Musharraf <mdmusharraf41125@gmail.com>
2026-07-17 22:38:35 +0530

    fix backend api

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ import {
   ChevronRight, Lock, Calendar, RefreshCw, Volume2
 } from 'lucide-react';
 
-const BACKEND_URL = import.meta.env.PROD ? 'https://hospital-automation-8u5m.onrender.com' : 'http://localhost:5000';
+const BACKEND_URL = import.meta.env.PROD ? 'https://hospital-automation-nc4h.onrender.com' : 'http://localhost:5000';
 
 // Global Socket Instance
 const socket = io(BACKEND_URL);
